9520a94c64301d5632f6189a9a8baeeead02a32d
Jord Visser <jordvisser@MacBook-Pro-van-Jord.local>
2018-02-17 00:37:38 +0100

    Made text output for departures and outputting all available departures for debug.

--- widget_9292.py
+++ widget_9292.py
@@ -37,17 +37,23 @@ with urllib.request.urlopen(departures_url + '?' + request_data) as response:
 departures_data = json.loads(response_data.decode(encoding))
 departures = departures_data['tabs'][0]['departures']
 departures_filtered_destination = [v for v in departures if v['destinationName'] == 'Gouda']
-departures_list = departures_filtered_destination[0:2]
+departures_list = departures_filtered_destination #[0:2]
 
 for depa in departures_list:
     dTime = dt.datetime.strptime('{} {}'.format(now.strftime('%Y-%m-%d'), depa['time']),'%Y-%m-%d %H:%M')
     if dTime < now:
         dTime = dTime + dt.timedelta(days=1)
-    if depa['realtimeState'] == 'late':
-        minutes = depa['realtimeText'].split(' ')[0]
-        dTime = dTime + dt.timedelta(seconds=int(minutes)*60)
+    if depa['realtimeState'] == 'ontime':
+        depText = 'op tijd'
+    elif depa['realtimeState'] == 'late':
+        minutes = int(depa['realtimeText'].split(' ')[0])
+        dTime = dTime + dt.timedelta(seconds=minutes*60)
+        depText = '{} minuten vertraagd'.format(minutes)
+    else:
+        depText = '--{}--'.format(depa['realtimeState'])
+
     deltaNow = dTime - now
-    pp.pprint(dTime)
-    pp.pprint(divmod(deltaNow.days * 86400 + deltaNow.seconds, 60))
+    deltaCalc = divmod(deltaNow.days * 86400 + deltaNow.seconds, 60)
+    print('Bus is {} en vertrekt om {} dat is over {} minuten.'.format(depText,dTime.strftime('%H:%M'),deltaCalc[0]))
 
 pp.pprint(departures_list)
