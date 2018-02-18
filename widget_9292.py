e32f5c04afa69f19974493f0631f280c2f100401
Jord Visser <jordvisser@gmail.com>
2018-02-18 22:24:49 +0100

    Trying another route to incoperate a message if no bus is departing in the given window. Color coding is now commented out.

--- widget_9292.py
+++ widget_9292.py
@@ -40,30 +40,38 @@ departures = departures_data['tabs'][0]['departures']
 departures_filtered_destination = [v for v in departures if v['destinationName'] == 'Gouda']
 departures_list = departures_filtered_destination #[0:2]
 
+departures_output = []
+
 for depa in departures_list:
     dTime = dt.datetime.strptime('{} {}'.format(now.strftime('%Y-%m-%d'), depa['time']),'%Y-%m-%d %H:%M')
     if dTime < now:
         dTime = dTime + dt.timedelta(days=1)
     if depa['realtimeState'] == 'ontime':
         depText = 'op tijd'
-        sys.stdout.write("\033[1;32m")
+        #sys.stdout.write("\033[1;32m")
     elif depa['realtimeState'] == 'late':
         minutes = int(depa['realtimeText'].split(' ')[0])
         dTime = dTime + dt.timedelta(seconds=minutes*60)
         depText = '{} minuten vertraagd'.format(minutes)
-        sys.stdout.write("\033[1;31m")
+        #sys.stdout.write("\033[1;31m")
     else:
         minutes = int(depa['realtimeText'].split(' ')[0])
         depText = '-- {} || {} min. --'.format(depa['realtimeState'], minutes)
-        sys.stdout.write("\033[38;5;208m")
+        #sys.stdout.write("\033[38;5;208m")
 
     deltaNow = dTime - now
     deltaCalc = divmod(deltaNow.days * 86400 + deltaNow.seconds, 60)
-    sys.stdout.write("\033[1m")
+    #sys.stdout.write("\033[1m")
     if deltaCalc[0] < 70:
-        print('Bus {} is {} en vertrekt over {} minuten om {}.'.format(depa['service'],depText,deltaCalc[0],dTime.strftime('%H:%M')))
+        departures_output.append('Bus {} is {} en vertrekt over {} minuten om {}.'.format(depa['service'],depText,deltaCalc[0],dTime.strftime('%H:%M')))
     else:
-        sys.stdout.write("\033[0m")
+        #sys.stdout.write("\033[0m")
         break
+
+if len(departures_output) == 0:
+    print('Er vertrekken geen bussen het komende uur.')
+else:
+    for depout in departures_output:
+        print(depout)
 
 pp.pprint(departures_list)
